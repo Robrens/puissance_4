326516242bd61fd8737cf412316460d64f83cd7a
kamilacase <xakdxshooter@live.fr>
2018-06-12 10:31:22 +0200

    init JS

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-var matrix = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]];
+// var matrix = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]];
+// Un objet representant la balise <div id="vide">
+var div = document.querySelector('#root');
+
+div.innerHTML = '';
+var tab = document.createElement('table');
+var row = document.createElement('tr');
+var column = document.createElement('td');
+tab.appendChild(div);
+row.appendChild(tab);
+column.appendChild(row);
